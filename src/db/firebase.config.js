600c25f6efb6a61d9d85382e961fc746af78f93e
MustafaRazaZuberi <mustafazuberi986@gmail.com>
2023-09-30 17:08:40 -0700

    feat : created basic app with firebase config
    now adding shadcn ui config in my app

---
+++ src/db/firebase.config.js
@@ -0,0 +1,24 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+const dbRef = (path) => {
+  return ref(db, path);
+};
+
+export { app, db, storage, auth, dbRef };
