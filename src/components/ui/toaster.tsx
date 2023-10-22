dffd5097d0fda9a4ea331663bceafa34114cab54
MustafaRazaZuberi <mustafazuberi986@gmail.com>
2023-10-22 10:38:10 -0700

    now working on chat

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   Toast,
@@ -7,11 +7,12 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+import Image from "next/image";
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts } = useToast();
 
   return (
     <ToastProvider>
@@ -21,15 +22,25 @@ export function Toaster() {
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
-                <ToastDescription>{description}</ToastDescription>
+                <ToastDescription className="flex flex-row gap-x-2 items-center justify-start">
+                  <Image
+                    src={
+                      "https://scontent.fkhi17-1.fna.fbcdn.net/v/t39.8562-6/120009688_325579128711709_1736249742330805861_n.png?_nc_cat=1&ccb=1-7&_nc_sid=f537c7&_nc_ohc=9LEzKei6YxkAX9z9op0&_nc_ht=scontent.fkhi17-1.fna&oh=00_AfCzp3zOJcQwXI3-OPY5LNEz6oHoA01gNMoZ5C3BreY6cA&oe=6539923D"
+                    }
+                    width={30}
+                    height={30}
+                    alt="messengerLogo"
+                  />
+                  <p>{description}</p>
+                </ToastDescription>
               )}
             </div>
             {action}
             <ToastClose />
           </Toast>
-        )
+        );
       })}
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
