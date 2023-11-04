6608c2532d312e298d37a5364052803cca5022ca
MustafaRazaZuberi <mustafazuberi986@gmail.com>
2023-11-03 21:37:00 -0700

    unsend message bug solved now optimizing code of unsend message message compoennt and forward message

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import TailwindSpinner from "@/customComponents/web/TailwindSpinner";
 
 export default function Loading() {
   return (
-    <main className="flex flex-col min-h-[90vh] border border-white items-center justify-center gap-y-3 ">
+    <main className="flex flex-col min-h-[90vh] items-center justify-center gap-y-3 ">
       <TailwindSpinner />
       <h3 className="text-2xl font-medium">Loading...</h3>
     </main>
